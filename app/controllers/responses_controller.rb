df554db85edb763edf0ed81207d45c4a7651ae11
Toby <toby.schaeffer@gmail.com>
2016-07-10 19:36:43 +0300

    added headers

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,6 +1,8 @@
 class ResponsesController < ApplicationController
 
+
 def index
+		headers['Access-Control-Allow-Origin'] = 'http://localhost:8080'
 		@responses = Response.get_api_info
 		# puts "@responses: #{@responses.inspect}"
 		puts "!!![count from api] @responses count: #{@responses.count}"
