56778fb18ecb8ffd5c01d0713118ff9c84c81e8a
Toby <toby.schaeffer@gmail.com>
2016-07-10 20:51:29 +0300

    changed headers

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,6 +1,12 @@
 class ResponsesController < ApplicationController
 def index
-		headers['Access-Control-Allow-Origin'] = 'http://localhost:8080/'
+		# headers['Access-Control-Allow-Origin'] = 'http://localhost:8080/'
+		add_header Access-Control-Allow-Headers "X-Requested-With";
+		add_header Access-Control-Allow-Methods "GET, HEAD, OPTIONS";
+		add_header Access-Control-Allow-Origin "*";
+
+
+		
 		@responses = Response.get_api_info
 		# puts "@responses: #{@responses.inspect}"
 		puts "!!![count from api] @responses count: #{@responses.count}"
