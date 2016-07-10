daa35a19ae7981eb97141c5c7a95049e8f8a0e32
Toby <toby.schaeffer@gmail.com>
2016-07-10 21:34:24 +0300

    cleaned up controller

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,6 +1,5 @@
 class ResponsesController < ApplicationController
 def index
-		# headers['Access-Control-Allow-Origin'] = 'http://localhost:8080/'
 		@responses = Response.get_api_info
 		# puts "@responses: #{@responses.inspect}"
 		puts "!!![count from api] @responses count: #{@responses.count}"
@@ -11,7 +10,7 @@ def index
     Response.find_or_create_by(record_id: response['_id'], lemurs_quantity: response['lemurs_quantity'], sighting_month: response['sighting_month'], sighting_year: response['sighting_year'], lemur_category: response['lemur_category'], location_admin1: response['location_admin1'], location_admin2: response['location_admin2'])
 
 		end
-		#i needed to update my render logic.  see addition of `each_serializer` here.  @responses has also changed to Response.all as well.
+		# render logic updated.  see addition of `each_serializer` here.  @responses has also changed to Response.all as well.
     render(	
       json: Response.all,
       each_serializer: ResponseSerializer
