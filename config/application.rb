bb563b61b734c93928701a3f1de7e55b02ff2cb6
Toby <toby.schaeffer@gmail.com>
2016-07-18 18:10:02 +0300

    minor update

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,7 @@ module Responses
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins 'https://petlemursurvey.github.io/'
-        # origins 'http://localhost:8080'
-        resource '*', :headers => :any, :methods => [:get, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     
     end
