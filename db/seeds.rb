cf8f1c8df7784915581a7be676f6f2f148b76c30
ts <toby.schaeffer@gmail.com>
2017-12-10 14:20:34 +0000

    modified seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,9 @@ Response.delete_all
   	#this pulls in new records that have been added to the api and adds them to the database.  it doesn't pull in records from the api that are already in the db
     
     # Response.find_or_create_by(lemurs_quantity: responses['lemurs_quantity'], month_and_year:  Date.parse(responses['month_and_year']), _geolocation: responses['_geolocation'].split(",").map { |s| s.to_i }, lemur_category: responses['lemur_category'], location_admin1: responses['location_admin1'], location_admin2: responses['location_admin2'])
-    Response.create(record_id: response['_id'], lemurs_quantity: response['lemurs_quantity'], sighting_month: response['sighting_month'], sighting_year: response['sighting_year'], lemur_category: response['lemur_category'], location_admin1: response['location_admin1'], location_admin2: response['location_admin2'])
+    # Response.create(record_id: response['_id'], lemurs_quantity: response['lemurs_quantity'], sighting_month: response['sighting_month'], sighting_year: response['sighting_year'], lemur_category: response['lemur_category'], location_admin1: response['location_admin1'], location_admin2: response['location_admin2'])    
+
+    Response.create(record_id: response['_id'], saw_captive_lemur: response['saw_captive_lemur'], lemurs_quantity: response['lemurs_quantity'], sighting_month: response['sighting_month'], sighting_year: response['sighting_year'], lemur_category: response['lemur_category'], location_admin1: response['location_admin1'], location_admin2: response['location_admin2'], environment: response['environment'], captive_environment: response['captive_environment'] )
 
 
   #some of my records don't have valid date values, so `rescue` is added to keep rails from returning a `notfound` page.  quick tutorial here:  http://www.rubytutorial.io/rails-rescue_from/
